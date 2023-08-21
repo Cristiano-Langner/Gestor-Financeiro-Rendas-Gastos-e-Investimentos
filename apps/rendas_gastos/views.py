cfa8b6254bfd80a1635c5de35271f2d5b9b22a9e
Cristiano-Langner <cristianolangner.dev@gmail.com>
2023-08-21 07:27:13 -0300

    att

--- apps/rendas_gastos/views.py
+++ apps/rendas_gastos/views.py
@@ -12,7 +12,43 @@ from apps.rendas_gastos.utils import check_authentication
 def index(request):
     if not check_authentication(request):
         return redirect('login')
-    return render(request, 'index.html')
+    else:
+        context_renda_gasto = rendas_gastos_view(request)
+        return render(request, 'index.html', context_renda_gasto)
+
+def rendas(request):
+    if not check_authentication(request):
+        return redirect('login')
+    else:
+        form = process_form(request, RendasForm, Rendas, 'rendas', 'Renda registrada com sucesso!')
+    rendas_cadastradas = Rendas.objects.filter(created_by=request.user)
+    categorias_renda = OpcoesRendas.choices
+    grafico_renda = graph(categorias_renda, rendas_cadastradas)
+    total_rendas, rendas_cadastradas = filter_selections(request, rendas_cadastradas)
+    rendas_cadastradas = rendas_cadastradas.order_by('-data')
+    paginator = Paginator(rendas_cadastradas, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'rendas_gastos/rendas.html', {'form': form, 'rendas_cadastradas': rendas_cadastradas,
+                    'total_rendas': total_rendas, 'opcoes_rendas': OpcoesRendas.choices, 'grafico_renda': grafico_renda,
+                    'opcoes_pagamentos': MetodoPagamento.choices, 'page_obj': page_obj})
+
+def gastos(request):
+    if not check_authentication(request):
+        return redirect('login')
+    else:
+        form = process_form(request, GastosForm, Gastos, 'gastos', 'Gasto registrado com sucesso!')
+    gastos_cadastrados = Gastos.objects.filter(created_by=request.user)
+    categorias = OpcoesGastos.choices
+    grafico_gasto = graph(categorias, gastos_cadastrados)
+    total_gastos, gastos_cadastrados = filter_selections(request, gastos_cadastrados)
+    gastos_cadastrados = gastos_cadastrados.order_by('-data')
+    paginator = Paginator(gastos_cadastrados, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'rendas_gastos/gastos.html', {'form': form, 'gastos_cadastrados': gastos_cadastrados,
+                    'total_gastos': total_gastos, 'opcoes_gastos': OpcoesGastos.choices, 'grafico_gasto': grafico_gasto,
+                    'opcoes_pagamentos': MetodoPagamento.choices, 'page_obj': page_obj})
 
 def process_form(request, form_class, created_class, reditect_name, success_message):
     if request.method == 'POST':
@@ -56,51 +92,21 @@ def filter_selections(request, name_cadastrado_categoria):
     name_cadastrado_categoria = name_cadastrado_categoria.filter(**filters)
     total = name_cadastrado_categoria.aggregate(total=Sum('valor'))['total']
     return total, name_cadastrado_categoria
+    
+def rendas_gastos_view(request):
+    today = datetime.today()
+    rendas_do_mes = Rendas.objects.filter(created_by=request.user, data__year=today.year, data__month=today.month)
+    renda_mes = sum(renda.valor for renda in rendas_do_mes)
+    gastos_do_mes = Gastos.objects.filter(created_by=request.user, data__year=today.year, data__month=today.month)
+    gasto_mes = sum(gasto.valor for gasto in gastos_do_mes)
+    saldo = renda_mes - gasto_mes
+    context = {
+        'renda_mes': renda_mes,
+        'gasto_total': gasto_mes,
+        'saldo': saldo,
+    }
+    return context
 
-def rendas(request):
-    if not check_authentication(request):
-        return redirect('login')
-    else:
-        form = process_form(request, RendasForm, Rendas, 'rendas', 'Renda registrada com sucesso!')
-    
-    rendas_cadastradas = Rendas.objects.filter(created_by=request.user)
-    categorias_renda = OpcoesRendas.choices
-    
-    grafico_renda = graph(categorias_renda, rendas_cadastradas)
-    
-    total_rendas, rendas_cadastradas = filter_selections(request, rendas_cadastradas)
-        
-    rendas_cadastradas = rendas_cadastradas.order_by('-data')
-    paginator = Paginator(rendas_cadastradas, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    
-    return render(request, 'rendas_gastos/rendas.html', {'form': form, 'rendas_cadastradas': rendas_cadastradas,
-                    'total_rendas': total_rendas, 'opcoes_rendas': OpcoesRendas.choices, 'grafico_renda': grafico_renda,
-                    'opcoes_pagamentos': MetodoPagamento.choices, 'page_obj': page_obj})
-
-def gastos(request):
-    if not check_authentication(request):
-        return redirect('login')
-    else:
-        form = process_form(request, GastosForm, Gastos, 'gastos', 'Gasto registrado com sucesso!')
-    
-    gastos_cadastrados = Gastos.objects.filter(created_by=request.user)
-    categorias = OpcoesGastos.choices
-    
-    grafico_gasto = graph(categorias, gastos_cadastrados)
-    
-    total_gastos, gastos_cadastrados = filter_selections(request, gastos_cadastrados)
-    
-    gastos_cadastrados = gastos_cadastrados.order_by('-data')
-    paginator = Paginator(gastos_cadastrados, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    
-    return render(request, 'rendas_gastos/gastos.html', {'form': form, 'gastos_cadastrados': gastos_cadastrados,
-                    'total_gastos': total_gastos, 'opcoes_gastos': OpcoesGastos.choices, 'grafico_gasto': grafico_gasto,
-                    'opcoes_pagamentos': MetodoPagamento.choices, 'page_obj': page_obj})
-    
 def delete_renda(request, renda_id):
     renda = get_object_or_404(Rendas, pk=renda_id)
     renda.delete()
