345ba00edd0d2dbb6490104c63be27a5ecc00629
Cristiano-Langner <cristianolangner.dev@gmail.com>
2023-11-01 14:28:36 -0300

    ajustes visuais

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -76,11 +76,13 @@ def config(request):
         else:
             form = TrocaSenhaForm()
         perguntas_respostas = [
-            {"pergunta": "Oquê estou vendo na página inicial?", "resposta": "Na página inicial você poderá conferir o resumo de suas rendas, gastos e dos seus investimentos através de tabelas e gráficos."},
+            {"pergunta": "O que estou vendo na página inicial?", "resposta": "Na página inicial você poderá conferir o resumo de suas rendas, gastos e dos seus investimentos através de tabelas e gráficos."},
             {"pergunta": "Para que serve o botão Consolidar Carteira na página inicial?", "resposta": "Serve para buscar os dados dos ativos dos seus investimentos cadastrados através do Yahoo Finance."},
-            {"pergunta": "A consolidação da carteira esta demorando muito, oque fazer?", "resposta": "É normal que a consolidação da carteira demore um pouco, mas caso esteja demorando mais que o normal, pode ser devido a alguma instabilidade na comunicação com os dados da Yahoo Finance, nesse caso recomenda-se atualizar a página e tentar novamente mais tarde."},
+            {"pergunta": "Como eu sei qual ticker colocar para o investimento?", "resposta": "O nome do ticker é para ser o mesmo nome utilizado para as negociações do ativo, em caso de dúvida pesquise na internet o nome do ticker do seu ativo em questão."},
+            {"pergunta": "A consolidação da carteira está demorando muito, o que fazer?", "resposta": "É normal que a consolidação da carteira demore um pouco, mas caso esteja demorando mais que o normal, pode ser devido a alguma instabilidade na comunicação com os dados da Yahoo Finance, nesse caso recomenda-se atualizar a página e tentar novamente mais tarde."},
+            {"pergunta": "Consolidei a carteira, mas alguns ou todos os valores estão zerados.", "resposta": "Geralmente isso acontece quando foi impossível obter os dados do servidor do Yahoo Finance, nesse caso recomenda-se tentar mais tarde. Também pode ser algum problema no nome do ticker cadastrado, por favor verifique se o ticker está correto."},
             {"pergunta": "Como funciona o cadastro de rendas e gastos?", "resposta": "No topo da página de rendas e gastos você verá os campos de informações a serem preenchidos, logo abaixo tem um botão de cadastrar renda ou gasto. Basta somente preencher as informações e clicar em cadastrar."},
-            {"pergunta": "Como funciona o filtro das rendas e gastos?", "resposta": "O filtro é bem simples, basta apenas escolher oque deseja filtrar e clicar no botão Filtrar, caso queira remover todos os filtros da pesquisa clique em Limpar Filtros."},
+            {"pergunta": "Como funciona o filtro das rendas e gastos?", "resposta": "O filtro é bem simples, basta apenas escolher o que deseja filtrar e clicar no botão Filtrar, caso queira remover todos os filtros da pesquisa clique em Limpar Filtros."},
         ]
     return render(request, 'usuarios/config.html', {'form': form, 'perguntas_respostas': perguntas_respostas})
 
